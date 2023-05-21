11c21488e42e4ab5e9dcd9004c874371246a648d
td2thinh <75013885+td2thinh@users.noreply.github.com>
2023-05-21 10:45:25 +0200

    Structure and Layout of the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import './App.css'
 import styles from "./style"
+import {
+  NavBar,
+  Billing,
+  Business,
+  Button,
+  CallToAction,
+  CardDeal,
+  Clients,
+  FeedBackCard,
+  Footer,
+  Hero,
+  GetStarted,
+  Stats,
+  Testimonials
+} from './components'
 
 const App = () => {
   return (
@@ -7,13 +22,23 @@ const App = () => {
       <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-            NAVBAR
+            <NavBar />
           </div>
         </div>
-
         <div className={`bg-primary ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            HERO
+            <Hero />
+          </div>
+        </div>
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <Testimonials />
+            <Clients />
+            <CallToAction />
+            <Footer />
           </div>
         </div>
       </div>
---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CallToAction from "./CallToAction";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";    
+import Testimonials from "./Testimonials";
+
+export {
+    NavBar,
+    Billing,
+    Business,
+    Button,
+    CallToAction,
+    CardDeal,
+    Clients,
+    FeedBackCard,
+    Footer,
+    Hero,
+    GetStarted,
+    Stats,
+    Testimonials
+}

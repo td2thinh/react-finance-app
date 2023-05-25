27a5ec05ab80d8bfa5fe0cbf037881c009a0a206
td2thinh <75013885+td2thinh@users.noreply.github.com>
2023-05-25 07:36:43 +0200

    Gradient and Robot hand added in Hero Component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,14 +27,37 @@ const Hero = () => {
                     <div className="flex flex-row  justify-between items-center w-full">
                         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px]">The Next {` `}
                             <br className="sm:block hidden"></br>
-                            <span className="text-lightAccent">Big Thing</span>
+
                             <br className="sm:block hidden"></br>
-                            {' '} is Here.
+                            {' '} Generation {` `}
                         </h1>
                         <div className="ss:flex hidden md:mr-4 mr-0">
                             <GetStarted />
                         </div>
                     </div>
+
+                    <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-lightAccent ss:leading-[100px] leading-[75px] w-full">
+                        Payment Processor
+                    </h1>
+
+                    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                        Our platform allows you to send money to anyone in the world, from anywhere in the world. Get started now!
+                    </p>
+                </div>
+
+                <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>                    <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+                    <div className="absolute z-[0] w-[40%] h-[35%] top-0" style={{
+                        "background": "linear-gradient(90deg, #f4c4c3 0%,  #fc67fa 100%)",
+                        "filter": "blur(900px)",
+                    }} />
+                    <div className="absolute z-[1] w-[80%] h-[85%] rounded-full bottom-40" style={{
+                        "background": "rgba(255, 255, 255, 0.6)",
+                        "filter": "blur(750px)",
+                    }} />
+                    <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-0" style={{
+                        "background": "linear-gradient(180deg, rgba(188, 165, 255, 0) 0%,  #214d76 100%)",
+                        "filter": "blur(900px)",
+                    }} />
                 </div>
             </section>
         </>

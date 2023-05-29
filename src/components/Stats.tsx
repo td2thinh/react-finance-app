ed2808d67e38c7adc203b88be95d1577d1029aa6
td2thinh <75013885+td2thinh@users.noreply.github.com>
2023-05-29 21:27:15 +0200

    Stats component done

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { stats } from "../constants/"
+import styles from "../style"
 
 const Stats = () => {
     return (
-        <div>Stats</div>
+        <>
+            <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+                {
+                    stats.map((item) => {
+                        return (
+                            <div key={item.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+                                <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]">
+                                    {item.value}
+                                </h4>
+                                <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-lightAccent uppercase ml-3">
+                                    {item.title}
+                                </p>
+                            </div>
+                        )
+                    })
+                }
+            </section>
+        </>
     )
 }
 
